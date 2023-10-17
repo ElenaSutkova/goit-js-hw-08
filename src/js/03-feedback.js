ae97fbce9c77a77d1f29eefb0207ebfcf752cc9c
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-10-17 11:09:49 +0300

    js6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,73 +11,59 @@
 // Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. 
 // Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
+
+
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
-
-const LS_KEY = "feedback-form-state";
-
+const LS_KEY = 'feedback-form-state';
 form.addEventListener('input', throttle(handlerInput, 500));
 form.addEventListener('submit', handlerSubmit);
 document.addEventListener('DOMContentLoaded', renderPage);
-
-let userData = localStorage.getItem(LS_KEY) || {};
-
+let userData = load(LS_KEY) || {};
 // function input
 function handlerInput(e) {
-    const target = e.target;
-    const formElValue = target.value;
-    const formElName = target.name;
-    userData[formElName] = formElValue;
-    localStorage.setItem(LS_KEY, JSON.stringify(userData));
+  const target = e.target;
+  const formElValue = target.value;
+  const formElName = target.name;
+  userData[formElName] = formElValue;
+  localStorage.setItem(LS_KEY, JSON.stringify(userData));
 }
-
-// function submit
-function handlerSubmit(event) {
-    event.preventDefault();
-    const { email, message } = event.currentTarget.elements;
-
-    if (!email.value || !message.value) {
-        alert("all fields must be filled")
-    }
-
-    data = {
-        email: email.value,
-        message: message.value
-    };
-
-    localStorage.getItem(LS_KEY);
-    console.log(data);
-
-    event.currentTarget.reset();
-    localStorage.removeItem(LS_KEY);
-}
-
-// function DOM
-function renderPage() {
-    const userDataFromLS = JSON.parse(localStorage.getItem(LS_KEY));
-
-    const load = key => {
+function load(key) {
   try {
     const serializedState = localStorage.getItem(key);
     return serializedState === null ? undefined : JSON.parse(serializedState);
   } catch (error) {
-    console.error("Get state error: ", error.message);
+    console.error('Get state error: ', error.message);
   }
-    };
-    
-    if (!userDataFromLS) {
-        return;
-    }
-    const formElements = form.elements;
-    for (const key in userDataFromLS) {
-        if (userDataFromLS.hasOwnProperty(key)) {
-            formElements[key].value = userDataFromLS[key];
-            if (userDataFromLS[key]) {
-                userData[key] = userDataFromLS[key]
-            }
-        }
-    }
 }
-
+// function submit
+function handlerSubmit(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+  if (!email.value || !message.value) {
+    return alert('all fields must be filled');
+  }
+  data = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(userData);
+  event.currentTarget.reset();
+  localStorage.removeItem(LS_KEY);
+}
+// function DOM
+function renderPage() {
+  if (!userData) {
+    return;
+  }
+  const formElements = form.elements;
+  for (const key in userData) {
+    if (userData.hasOwnProperty(key)) {
+      formElements[key].value = userData[key];
+      if (userData[key]) {
+        userData[key] = userData[key];
+      }
+    }
+  }
+}
 
